14a04d1a740d88c8f0e45e2f9ad4b8f9799bba17
JPrime <132490938+MLGJPrime@users.noreply.github.com>
2023-09-21 19:14:21 +0200

    Tried basic plotting tehniques

--- plotting.py
+++ plotting.py
@@ -1,8 +1,50 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([0, 6])
-y = np.array([3, 10])
+import matplotlib as mpl
 
-plt.plot(x, y)
+# b = np.matrix([[1, 2], [3, 4]])
+# b_asarray = np.asarray(b)
+
+# fig, ax = plt.subplots()  # Create a figure containing a single axes.
+# ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
+# plt.show()
+
+
+# np.random.seed(19680801)  # seed the random number generator.
+# data = {'a': np.arange(50),
+#         'c': np.random.randint(0, 50, 50),
+#         'd': np.random.randn(50)}
+# data['b'] = data['a'] + 10 * np.random.randn(50)
+# data['d'] = np.abs(data['d']) * 100
+
+# fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+# ax.scatter('a', 'b', c='c', s='d', data=data)
+# ax.set_xlabel('entry a')
+# ax.set_ylabel('entry b')
+# plt.show()
+
+# x = np.linspace(0, 2, 100)  # Sample data.
+
+# # Note that even in the OO-style, we use `.pyplot.figure` to create the Figure.
+# fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+# ax.plot(x, x, label='linear')  # Plot some data on the axes.
+# ax.plot(x, x**2, label='quadratic')  # Plot more data on the axes...
+# ax.plot(x, x**3, label='cubic')  # ... and some more.
+# ax.set_xlabel('x label')  # Add an x-label to the axes.
+# ax.set_ylabel('y label')  # Add a y-label to the axes.
+# ax.set_title("Simple Plot")  # Add a title to the axes.
+# ax.legend()  # Add a legend.
+# plt.show()
+
+x = np.linspace(0, 2, 100)  # Sample data.
+
+plt.figure(figsize=(5, 2.7), layout='constrained')
+plt.plot(x, x, label='linear')  # Plot some data on the (implicit) axes.
+plt.plot(x, x**2, label='quadratic')  # etc.
+plt.plot(x, x**3, label='cubic')
+plt.xlabel('x label')
+plt.ylabel('y label')
+plt.title("Simple Plot")
+plt.legend()
 plt.show()
